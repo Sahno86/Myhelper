a0da223b9e8a5ebd8ee2720c56423a181fef0bcc
sahno <mishenkinav@gmail.com>
2023-11-05 20:48:07 +0500

    добавлены, но не настроены кнопки добавления и удаления todo

--- myhelper/todo/views.py
+++ myhelper/todo/views.py
@@ -4,3 +4,11 @@ from .models import ToDo
 def todo(request):
     table = ToDo.objects.all()
     return render(request, 'todo/todo.html', {'table': table})
+
+def add_todo(request):
+    add_button_test = request.GET.get("add")
+    return print(add_button_test)
+
+def del_todo(request):
+    del_button_test = request.GET.get("del")
+    return HttpResponse(f'{del_button_test}Сработала кнопка удаления')
