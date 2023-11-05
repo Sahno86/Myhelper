59cb68ab04ed2303592139c38ea892a91f11e454
sahno <mishenkinav@gmail.com>
2023-11-05 20:01:00 +0500

    добавлен шаблон main, переделан views на render

--- myhelper/main_page/views.py
+++ myhelper/main_page/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def main(request):
-    return HttpResponse('Переход на <a href=http://127.0.0.1:8000/todo/>todo</a>')
+    # return HttpResponse('<a href=http://127.0.0.1:8000/todo/>Переход на todo</a>'
+    #                     '<br>'
+    #                     '<a href=http://127.0.0.1:8000/admin/>Админка</a>')
+    return render(request, 'main_page/main.html')
