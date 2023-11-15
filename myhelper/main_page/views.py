7fdf7227bf3b2992fdd2a9b12aab2cb0d48ff843
sahno <mishenkinav@gmail.com>
2023-11-15 22:55:19 +0500

    подключен bootstrap для todo.html

--- myhelper/main_page/views.py
+++ myhelper/main_page/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 
 
 def main(request):
-    # return HttpResponse('<a href=http://127.0.0.1:8000/todo/>Переход на todo</a>'
-    #                     '<br>'
-    #                     '<a href=http://127.0.0.1:8000/admin/>Админка</a>')
     return render(request, 'main_page/main.html', {'title': 'Главная страница'})
