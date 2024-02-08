582873520eaaa8cd2643a9a7449225c3881ee8ad
sahno <Mishenkinav@gmail.com>
2024-02-08 13:58:59 +0500

    попытки привязать шаблон

--- myhelper/todo/urls.py
+++ myhelper/todo/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', ToDo.as_view(), name='todo'),
+    path('todo_list', ToDoList.as_view(), name='todo_list'),
     path('add_todo/', add_todo, name='add_todo'),
     path('del_todo/', del_todo, name='del_todo')
 ]
