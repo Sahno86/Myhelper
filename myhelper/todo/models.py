71457b1c8b0773e0aa6ac36b0bd19506258df0e4
sahno <Mishenkinav@gmail.com>
2024-02-11 15:40:42 +0500

    привязал todo к главной странице, настроил работу кнопки добавить задание

--- myhelper/todo/models.py
+++ myhelper/todo/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class ToDo(models.Model):
     title = models.CharField('Название', max_length=100, name='title')
     is_done = models.BooleanField('Завершено', default=False, name='is_done')
--- myhelper/todo/views.py
+++ myhelper/todo/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, ListView
 
-from .models import *
+from . import models
+from .models import ToDo
 from django.views.decorators.http import require_http_methods
 
 
@@ -11,24 +12,10 @@ class ToDo(TemplateView):
     template_name = 'todo/todo.html'
 
 
-class ToDoList(TemplateView):
-    template_name = 'todo/list_todo.html'
-    # model = ToDo
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     return context
-
-
-# def todo(request):
-#     todo_obj = ToDo.objects.all()
-#     return render(request, 'todo/todo.html', {'todo_obj': todo_obj, 'title': 'todo'})
-
-
 @require_http_methods(["POST"])
 def add_todo(request):
     if request.method == "POST":
-        add_todo_task = ToDo()
+        add_todo_task = models.ToDo()
         add_todo_task.title = request.POST.get("add")
         add_todo_task.is_done = False
         add_todo_task.save()
