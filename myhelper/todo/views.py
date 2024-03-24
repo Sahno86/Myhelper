bf8a3a12aa1533f163a216f1cb745b31d5686f11
sahno <Mishenkinav@gmail.com>
2024-03-24 21:59:54 +0500

    удалено все лишнее

--- myhelper/todo/views.py
+++ myhelper/todo/views.py
@@ -27,25 +27,5 @@ class SuccesAddToDo(TemplateView):
     template_name = 'todo/succes_add_todo.html'
 
 
-# def add_todo(request):
-#     # if this is a POST request we need to process the form data
-#     if request.method == "POST":
-#         # create a form instance and populate it with data from the request:
-#         form = ToDoForm(request.POST)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             # process the data in form.cleaned_data as required
-#             # ...
-#             form.save()
-#             # redirect to a new URL:
-#             return render(request, "todo/todo.html")
-#
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = ToDoForm()
-#
-#     return render(request, "todo/todo_form.html", {"form": form})
-
-
 def del_todo(request):
     pass
